6c556dfa5c75eb8aa21ffca0a6cf05c16b614c36
Chirag <yadaavchirag@gmail.com>
2024-11-27 20:49:57 +0530

    user login done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,7 +6,12 @@ const userController = require("../controllers/user.controller")
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must be at least 3 characters long'),
-    body('password').isLength({min: 6}).withMessage("Password must be atleast 6 Characters")
-], userController.registerUser )
+    body('password').isLength({ min: 6 }).withMessage("Password must be atleast 6 Characters")
+], userController.registerUser)
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage("Password must be atleast 6 Characters")
+], userController.loginUser);
 
 module.exports = router;
